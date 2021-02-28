b299627ff3c21fceb46127647e19c2a47bcb5f41
Bruno <brunoco56@hotmail.com>
2021-02-28 13:02:36 -0300

    Configuração do appsettings

---
+++ DAL/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaVendas.Entidades;
+
+namespace SistemaVendas.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Venda> Venda { get; set; }
+        public DbSet<VendaProdutos> VendaProdutos { get; set; }
+
+
+        // Construtor que chama o Banco de Dados
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
