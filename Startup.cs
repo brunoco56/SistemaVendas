2697aa47b00d5bd3c6c251786501cd500cc487f2
Bruno <brunoco56@hotmail.com>
2021-02-28 13:11:12 -0300

    Configuração do Contexto no Startup.Cs

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SistemaVendas.DAL;
 
 namespace SistemaVendas
 {
@@ -24,6 +21,11 @@ namespace SistemaVendas
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddConnections();
+            services.AddSession();            
+            // Setting the Database 
+            services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlServer(Configuration.GetConnectionString("AppCon")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
